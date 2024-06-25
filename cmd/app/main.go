1780862a89a0b9736073edf93532853547430a21
cookie_jar <cptpricecod098@gmail.com>
2024-06-25 11:50:26 +0530

    add and sell features implemented

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	app "github.com/dibyajyoti-mandal/cli-app"
+)
+
+const (
+	dataFile = ".data.json"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	add := flag.Bool("add", false, "add new item")
+	sell := flag.Int("sell", 0, "mark item sold")
+	flag.Parse()
+	items := &app.Items{}
+	if err := items.Load(dataFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	switch {
+	case *add:
+		items.Add("Book 2", 150)
+		err := items.Write(dataFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *sell > 0:
+		err := items.Sold(*sell)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = items.Write(dataFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "Invalid Command")
+		os.Exit(0)
+
+	}
+
 }
+
+//add - add item by passing name and price
+//sell=idx sell item at index = idx
